c288192797166f983075412a77605a471b2ce913
RnWater <RnWater@aliyun.com>
2019-05-08 16:56:31 +0800

    增加透明activity

--- imagepicker/src/main/java/com/rx/img/activity/RxTranslucentActivity.java
+++ imagepicker/src/main/java/com/rx/img/activity/RxTranslucentActivity.java
@@ -1,15 +1,35 @@
 package com.rx.img.activity;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.rx.img.R;
 
-public class RxTranslucentActivity extends AppCompatActivity {
+import java.util.List;
+
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class RxTranslucentActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx_translucent);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
